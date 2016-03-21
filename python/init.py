307ce10ae2dd364a4cee01e2284999c73b47755d
Sam Zagrobelny <strangesast@gmail.com>
2016-03-21 16:33:30 -0400

    added basic config

--- python/init.py
+++ python/init.py
@@ -57,7 +57,7 @@ def setup_sim(tty_future):
 
     proc = yield from create
 
-    all_data = yield from get_line_until_timeout(proc)
+    all_data = yield from get_line_until_timeout(proc, 1.5, False)
 
     pts = find_match_in_iter(all_data, pat)
 
@@ -68,7 +68,11 @@ def setup_sim(tty_future):
         print('FAILED')
         tty_future.set_exception(Exception('no pts returned'))
 
-    yield from proc.wait()
+    try:
+        yield from proc.wait()
+    except asyncio.CancelledError:
+        print('simulator canceled')
+        pass
 
     return
 
@@ -81,16 +85,18 @@ def upload_firmware():
 
 @asyncio.coroutine
 def make_and_upload_sim_code(sim_loc):
-    print('building firmware...')
+    print('building firmware...', end='')
 
     make_command = ['make']
     make_path = '../arduino/testing'
     make_create = create_process_with_command(make_command, make_path)
     proc = yield from make_create
-    all_data = yield from get_line_until_timeout(proc)#, 2.0, True)
+    all_data = yield from get_line_until_timeout(proc, 1.5, False)
     exit_code = yield from proc.wait()
 
-    print('uploading firmware...')
+    print('DONE')
+
+    print('uploading firmware...', end='')
 
     upload_command = unbuf + ['avrdude', '-p', 'm328p', '-c', 'arduino',
             '-P', sim_loc, '-U', 'flash:w:build-uno/testing.hex']
@@ -98,25 +104,29 @@ def make_and_upload_sim_code(sim_loc):
     upload_path = '../arduino/testing'
     create = create_process_with_command(upload_command, upload_path)
     proc = yield from create
-    all_data = yield from get_line_until_timeout(proc)#, 2.0, True)
+    all_data = yield from get_line_until_timeout(proc, 1.5, False)
 
     exit_code = yield from proc.wait()
+
+    assert exit_code == 0
 
     print('DONE')
     return exit_code
 
 
-def start_serial_connection(name, pts, connection_made_future):
+def start_serial_connection(keg, pts):
     print('starting serial connection with {}...'.format(repr(pts)))
-
-    yield from serial.aio.create_serial_connection(
+    connection_made_future = asyncio.Future()
+    serial_transport = yield from serial.aio.create_serial_connection(
             asyncio.get_event_loop(),
-            lambda: SerialProtocol(name, pts, connection_made_future),
+            lambda: SerialProtocol(keg, pts, connection_made_future),
             port=pts,
             baudrate=9600)
 
     print('waiting for connection...')
-    return (yield from connection_made_future)
+    yield from connection_made_future
+
+    return serial_transport
 
 
 def parse_full_line(port_from, line):
@@ -140,9 +150,9 @@ def parse_full_line(port_from, line):
 
 
 class SerialProtocol(asyncio.Protocol):
-    def __init__(self, name, port, connection_established_future):
+    def __init__(self, keg, port, connection_established_future):
         self.port = port
-        self.name = name
+        self.keg = keg
         self.connection_established_future = connection_established_future
         self.current_data = bytearray()
 
@@ -151,7 +161,7 @@ class SerialProtocol(asyncio.Protocol):
             self.connection_established_future.set_result(True)
         self.transport = transport
         SerialProtocol.items.append(self)
-        print('serial port opened: {} ({})'.format(repr(self.name), self.port))
+        print('serial port opened: {} ({})'.format(repr(self.keg.name), self.port))
 
     def data_received(self, data):
         self.current_data += data
@@ -159,15 +169,16 @@ class SerialProtocol(asyncio.Protocol):
         spl = parsed.split('\n')
         while len(spl) > 1:
             line = spl.pop(0)
-            result = parse_full_line(self.port, line)
-            #text = 'line from {} ({}): {}'.format(repr(self.name), self.port, repr(line))
+            self.keg.message_received(line)
+            #result = parse_full_line(self.port, line)
+            #text = 'line from {} ({}): {}'.format(repr(self.keg.name), self.port, repr(line))
 
-            for protocol in SocketProtocol.items:
-                transport = protocol.transport
-                t = result.get('type')
-                if not (t is None or t == 'unknown'):
-                    transport.write(json.dumps(result).encode())
-                    transport.write(b'\n')
+            #for protocol in SocketProtocol.items:
+            #    transport = protocol.transport
+            #    t = result.get('type')
+            #    if not (t is None or t == 'unknown'):
+            #        transport.write(json.dumps(result).encode())
+            #        transport.write(b'\n')
 
         self.current_data = spl[0].encode()
 
@@ -177,6 +188,7 @@ class SerialProtocol(asyncio.Protocol):
 
     items = []
 
+@asyncio.coroutine
 def get_temps(delay):
     while True:
         print('sleeping for {}s'.format(delay))
@@ -187,6 +199,7 @@ def get_temps(delay):
             transport.write(b'temps\n')
 
     return
+
 
 class SocketProtocol(asyncio.Protocol):
     def __init__(self):
@@ -200,23 +213,33 @@ class SocketProtocol(asyncio.Protocol):
 
     def data_received(self, data):
         message = data.decode()
-        print('message received: {}'.format(repr(message)))
 
-        #if message[:5] == 'close':
-        #    self.transport.write(b'closing...\n')
-        #    self.transport.close()
-        #    return
+        m1 = re.findall('reg\s(\/dev\/[\w\/]+)', message)
+        if m1:
+            keg = Keg.get_keg_by_pts(m1[0])
+            if keg is None:
+                text = 'keg with pts {} not found\n'.format(repr(m1[0]))
 
-        #elif message[:4] == 'quit':
-        #    self.transport.write(b'stopping...\n')
-        #    loop.stop()
-        #    return
+            else:
+                keg.registered_to.add(self)
+                text = 'registered to {}\n'.format(repr(keg.name))
 
-        #for sertransport in serial_transports:
-        #    print('writing to serial...')
-        #    sertransport.write(message.encode())
+            return self.transport.write(text.encode())
 
-        #self.transport.write(b'wrote: ' + data)
+        m2 = re.findall('reg\s"([\w\s]+)"', message)
+        if m2:
+            keg = Keg.get_keg_by_name(m2[0])
+            if keg is None:
+                text = 'keg with name {} not found\n'.format(repr(m2[0]))
+
+            else:
+                keg.registered_to.add(self)
+                text = 'registered to {}\n'.format(repr(keg.name))
+
+            return self.transport.write(text.encode())
+
+        self.transport.write(b'unrecognized or malformed command\n')
+        
 
     def connection_lost(self, exc):
         peername = self.transport.get_extra_info('peername')
@@ -227,30 +250,113 @@ class SocketProtocol(asyncio.Protocol):
     items = []
 
 
+class Keg:
+    def __init__(self, name, virtual=True, host=None, port=None):
+        self.name = name
+        self.host = host
+        self.port = port
+        self.virtual = virtual
+        self.connection_ready = asyncio.Future()
+        self.registered_to = set()
+        Keg.items.append(self)
+
+    @asyncio.coroutine
+    def init_connection(self):
+        # establish connection
+        print('establishing connection for {}'.format(repr(self.name)))
+
+        if self.virtual:
+            sim_setup_complete = asyncio.Future()
+            sim_task = asyncio.ensure_future(setup_sim(sim_setup_complete))
+
+            port_defs = yield from sim_setup_complete
+
+            sim, pts = [port_defs.get(x) for x in ['sim', 'pts']]
+            self.pts = pts
+            yield from make_and_upload_sim_code(sim)
+
+            transport, protocol = yield from start_serial_connection(self, pts)
+
+            self.serial_transport = transport
+
+        print('connected!')
+
+    def send_message(self, message):
+        message = message.rstrip('\n') + '\n'
+        self.serial_transport.write(message.encode())
+
+    def message_received(self, message):
+        if 'welcome' in message:
+            # should happen only once
+            print('ready...')
+            self.connection_ready.set_result(True)
+        else:
+            print('message for {}: {}'.format(repr(self.name), repr(message)))
+            for protocol in self.registered_to:
+                try:
+                    protocol.transport.write("message from {}: {}\n".format(repr(self.name), message).encode())
+                # too vague
+                except:
+                    print('removing transport...')
+                    self.registered_to.remove(protocol)
+
+
+    @classmethod
+    def get_keg_by_name(cls, name):
+        return next((keg for keg in cls.items if keg.name == name), None)
+
+    @classmethod
+    def get_keg_by_pts(cls, pts):
+        return next((keg for keg in cls.items if keg.pts == pts), None)
+
+    items = []
+
+
 @asyncio.coroutine
 def main():
-    # future for pts retrieval
-    fut = asyncio.Future()
+    with open('config.json') as f:
+        config = json.load(f)
 
-    # run this in the background until process terminates,
-    # then possibly restart it
-    simulator_proc = asyncio.ensure_future(setup_sim(fut))
+    for kegid, val in config['kegs'].items():
+        if val.get('virtual') or kegid == 'test':
+            keg = Keg(val['name'], True)
+            yield from keg.init_connection()
 
-    locs = yield from fut
-    pts = locs['pts']
-    sim = locs['sim']
+            yield from keg.connection_ready
 
-    yield from make_and_upload_sim_code(sim)
+            keg.send_message('temps')
 
-    con_fut = asyncio.Future()
+        else:
+            pass
 
-    name = 'testing port'
-    yield from asyncio.ensure_future(start_serial_connection(name, pts, con_fut))
+    while True:
+        yield from asyncio.sleep(10.0)
+        for keg in Keg.items:
+            yield from asyncio.sleep(1.0)
+            keg.send_message('temps\n')
 
-    # repeatedly get temps every 10s
-    yield from asyncio.ensure_future(get_temps(10))
+    ## future for pts retrieval
+    #fut = asyncio.Future()
 
-    yield from simulator_proc
+    ## run this in the background until process terminates,
+    ## then possibly restart it
+    #simulator_proc = asyncio.ensure_future(setup_sim(fut))
+
+    #locs = yield from fut
+    #pts = locs['pts']
+    #sim = locs['sim']
+
+    #yield from make_and_upload_sim_code(sim)
+
+    #con_fut = asyncio.Future()
+
+    #name = 'testing port'
+    #yield from asyncio.ensure_future(start_serial_connection(name, pts, con_fut))
+
+    ## repeatedly get temps every 10s
+    #temps_coro = get_temps(10)
+    #all_temps_task = yield from asyncio.ensure_future(temps_coro)
+    #yield from simulator_proc
 
 
 loop = asyncio.get_event_loop()
